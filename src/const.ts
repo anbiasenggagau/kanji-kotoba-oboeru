a387fffbb2efcd010c72b1fa14171796567ad437
anbiasenggagau <muhammadsenggagau@gmail.com>
2025-10-21 07:37:28 +0700

    -

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 // available volumes for each level (example)
 export const volumes: Record<string, number[]> = {
     N5: [1, 2, 3, 4, 5, 6],
-    N4: [1, 2, 3, 4, 5],
+    N4: [1, 2, 3, 4, 5, 6, 7],
     // N3: [1, 2, 3, 4, 5, 6, 7]
 }
